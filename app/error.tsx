8042d26a746607c4010104e5af9f9f11ab6cf882
Maria <maria@Marias-MacBook-Pro.local>
2024-03-25 22:29:03 +0200

    fix

--- app/error.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import Heading from "@/components/heading";
-
-const Error: React.FC = () => {
-  return <Heading text="Oops! Some error :(" />
-}
- 
-export default Error;
--- types/social.ts
+++
@@ -1,5 +0,0 @@
-export interface ISocial {
-    id: number;
-    icon: string;
-    path: string;
-}
